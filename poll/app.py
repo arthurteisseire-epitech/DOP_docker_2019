5c259e5a3e7df06fee913251a796c8cad2b2fe00
arthur <arthur.teisseire@epitech.eu>
2019-10-31 15:18:52 +0100

    Add environment variable to get Hostnames

--- poll/app.py
+++ poll/app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host="redis", db=0, socket_timeout=5)
+        g.redis = Redis(host=os.environ['REDIS_HOSTNAME'], db=0, socket_timeout=5)
     return g.redis
 
 
